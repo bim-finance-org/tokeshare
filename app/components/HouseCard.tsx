ea0156f1b2013f7a3aae8ebc2bfc7924b0ef662c
Noe ROUMEAS <noe.roumeas@outlook.fr>
2025-01-28 17:00:20 +0100

    lint

--- app/components/HouseCard.tsx
+++ app/components/HouseCard.tsx
@@ -6,78 +6,98 @@ import SurfaceIcon from "./icons/SurfaceIcon";
 import BedIcon from "./icons/BedIcon";
 import ArrowIcon from "./icons/ArrowIcon";
 import Image from "next/image";
-import Link from "next/link";
 
 interface HouseCardProps {
-  name: string;
-  beds: number;
-  surface: number;
-  price: number;
-  city: string;
-  image: string;
-  link: string;
+	name: string;
+	beds: number;
+	surface: number;
+	price: number;
+	city: string;
+	image: string;
 }
 
-const HouseCard: React.FC<HouseCardProps> = ({ name, beds, surface, price, city, image, link }) => {
-  const cardRef = useRef<HTMLDivElement>(null);
-  const [isColumn, setIsColumn] = useState(true);
+const HouseCard: React.FC<HouseCardProps> = ({
+	name,
+	beds,
+	surface,
+	price,
+	city,
+	image,
+}) => {
+	const cardRef = useRef<HTMLDivElement>(null);
+	const [isColumn, setIsColumn] = useState(true);
 
-  useEffect(() => {
-    const observer = new ResizeObserver((entries) => {
-      if (entries[0]) {
-        const width = entries[0].contentRect.width;
-        setIsColumn(width < 350);
-      }
-    });
+	useEffect(() => {
+		const observer = new ResizeObserver((entries) => {
+			if (entries[0]) {
+				const width = entries[0].contentRect.width;
+				setIsColumn(width < 350);
+			}
+		});
 
-    if (cardRef.current) {
-      observer.observe(cardRef.current);
-    }
+		if (cardRef.current) {
+			observer.observe(cardRef.current);
+		}
 
-    return () => observer.disconnect();
-  }, []);
+		return () => observer.disconnect();
+	}, []);
 
-  return (
-    <div
-      ref={cardRef}
-      className="bg-color5 text-color4 text-base sm:text-lg shadow-lg rounded-xl overflow-hidden flex flex-col min-w-0 
-                    max-w-[90%] sm:max-w-full mx-auto"
-    >
-      <Image src={image} alt="House" width={800} height={800} className="object-cover" />
-      <div className="p-3 sm:p-4 flex flex-col flex-grow">
-        <div>
-          <h3 className="font-semibold text-lg sm:text-xl">{name}</h3>
-          <div className="flex flex-wrap items-center space-x-3 mt-2 text-sm sm:text-base">
-            <p className="flex items-center">
-              <BedIcon size={36} />
-              {beds} bed{beds > 1 && "s"}
-            </p>
-            <p className="flex items-center">
-              <SurfaceIcon />
-              {surface} m²
-            </p>
-            <p className="flex items-center">
-              <LocationIcon />
-              {city}
-            </p>
-          </div>
-        </div>
+	return (
+		<div
+			ref={cardRef}
+			className='bg-color5 text-color4 text-base sm:text-lg shadow-lg rounded-xl overflow-hidden flex flex-col min-w-0 
+                    max-w-[90%] sm:max-w-full mx-auto'
+		>
+			<Image
+				src={image}
+				alt='House'
+				width={800}
+				height={800}
+				className='object-cover'
+			/>
+			<div className='p-3 sm:p-4 flex flex-col flex-grow'>
+				<div>
+					<h3 className='font-semibold text-lg sm:text-xl'>{name}</h3>
+					<div className='flex flex-wrap items-center space-x-3 mt-2 text-sm sm:text-base'>
+						<p className='flex items-center'>
+							<BedIcon size={36} />
+							{beds} bed{beds > 1 && "s"}
+						</p>
+						<p className='flex items-center'>
+							<SurfaceIcon />
+							{surface} m²
+						</p>
+						<p className='flex items-center'>
+							<LocationIcon />
+							{city}
+						</p>
+					</div>
+				</div>
 
-        <div className={`mt-4 flex ${isColumn ? "flex-col items-start" : "flex-row items-center"} justify-between gap-2 sm:gap-4`}>
-          <h5 className="text-blue-600 font-bold text-lg sm:text-xl">{price.toLocaleString("en-US", { style: "currency", currency: "USD" })}</h5>
+				<div
+					className={`mt-4 flex ${
+						isColumn ? "flex-col items-start" : "flex-row items-center"
+					} justify-between gap-2 sm:gap-4`}
+				>
+					<h5 className='text-blue-600 font-bold text-lg sm:text-xl'>
+						{price.toLocaleString("en-US", {
+							style: "currency",
+							currency: "USD",
+						})}
+					</h5>
 
-          {/* <Link href={link} > */}
-          <button className="px-8 w-full sm:w-auto bg-color4 text-white rounded-lg flex items-center justify-center whitespace-nowrap hover:bg-color2">
-            <h5>Available Soon</h5>
-            <span className="ml-2">
-              <ArrowIcon size={24} />
-            </span>
-          </button>
-          {/* </Link> */}
-        </div>
-      </div>
-    </div>
-  );
+					{/* <Link href={link} > */}
+					<button className='px-8 w-full sm:w-auto bg-color4 text-white rounded-lg flex items-center justify-center whitespace-nowrap hover:bg-color2'>
+						<h5>Available Soon</h5>
+						<span className='ml-2'>
+							<ArrowIcon size={24} />
+						</span>
+					</button>
+					{/* </Link> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HouseCard;
--- app/components/HouseCardPlus.tsx
+++ app/components/HouseCardPlus.tsx
@@ -5,123 +5,154 @@ import BedIcon from "./icons/BedIcon";
 import SurfaceIcon from "./icons/SurfaceIcon";
 import LocationIcon from "./icons/LocationIcon";
 import ArrowIcon from "./icons/ArrowIcon";
-import Link from "next/link";
 import Image from "next/image";
 
 interface HomeCardPlusProps {
-  name: string;
-  beds: number;
-  surface: number;
-  price: number;
-  city: string;
-  image: string;
-  link: string;
-  tokenPrice: number;
-  expectedIncome: number;
-  dateIncome: string;
-  tokenIncome: number;
+	name: string;
+	beds: number;
+	surface: number;
+	price: number;
+	city: string;
+	image: string;
+	link: string;
+	tokenPrice: number;
+	expectedIncome: number;
+	dateIncome: string;
+	tokenIncome: number;
 }
 
-const HomeCardPlus: React.FC<HomeCardPlusProps> = ({ name, beds, surface, price, city, image, link, tokenPrice, expectedIncome, dateIncome, tokenIncome }) => {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const [isCompact, setIsCompact] = useState(false);
-  const [formattedPrice, setFormattedPrice] = useState<string | null>(null);
-  const [formattedTokenPrice, setFormattedTokenPrice] = useState<string | null>(null);
+const HomeCardPlus: React.FC<HomeCardPlusProps> = ({
+	name,
+	beds,
+	surface,
+	price,
+	city,
+	image,
+	tokenPrice,
+	expectedIncome,
+	dateIncome,
+	tokenIncome,
+}) => {
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const [isCompact, setIsCompact] = useState(false);
+	const [formattedPrice, setFormattedPrice] = useState<string | null>(null);
+	const [formattedTokenPrice, setFormattedTokenPrice] = useState<string | null>(
+		null
+	);
 
-  useEffect(() => {
-    // S'assurer que la mise en forme des nombres se fait uniquement côté client
-    setFormattedPrice(price.toLocaleString("en-US"));
-    setFormattedTokenPrice(tokenPrice.toLocaleString("en-US"));
-  }, [price, tokenPrice]);
+	useEffect(() => {
+		// S'assurer que la mise en forme des nombres se fait uniquement côté client
+		setFormattedPrice(price.toLocaleString("en-US"));
+		setFormattedTokenPrice(tokenPrice.toLocaleString("en-US"));
+	}, [price, tokenPrice]);
 
-  useEffect(() => {
-    const observer = new ResizeObserver((entries) => {
-      if (entries.length > 0) {
-        const width = entries[0].contentRect.width;
-        setIsCompact(width < 500);
-      }
-    });
+	useEffect(() => {
+		const observer = new ResizeObserver((entries) => {
+			if (entries.length > 0) {
+				const width = entries[0].contentRect.width;
+				setIsCompact(width < 500);
+			}
+		});
 
-    const containerElement = containerRef.current;
+		const containerElement = containerRef.current;
 
-    if (containerElement) {
-      observer.observe(containerElement);
-    }
+		if (containerElement) {
+			observer.observe(containerElement);
+		}
 
-    return () => {
-      if (containerElement) {
-        observer.unobserve(containerElement);
-      }
-    };
-  }, []);
+		return () => {
+			if (containerElement) {
+				observer.unobserve(containerElement);
+			}
+		};
+	}, []);
 
-  return (
-    <div ref={containerRef} className="bg-color1 shadow-lg max-w-xl rounded-3xl">
-      <Image src={image} alt={`Image of ${name}`} width={500} height={400} className="w-full h-auto object-cover rounded-t-3xl" />
+	return (
+		<div
+			ref={containerRef}
+			className='bg-color1 shadow-lg max-w-xl rounded-3xl'
+		>
+			<Image
+				src={image}
+				alt={`Image of ${name}`}
+				width={500}
+				height={400}
+				className='w-full h-auto object-cover rounded-t-3xl'
+			/>
 
-      <div className="px-8 py-4">
-        <div className="flex items-center w-full">
-          <div className="border-t-2 border-color4 w-8 mr-2 ml-1"></div>
-          <h1 className="text-color4 text-2xl">{name}</h1>
-        </div>
-        <div className={`text-color4 text-lg flex items-center ${isCompact ? "flex-col space-y-2 items-start" : "space-x-4"} mt-2`}>
-          <p className={`flex items-center ${isCompact ? "w-full" : ""} `}>
-            <span className="mr-1">
-              <BedIcon size={36} />
-            </span>
-            {beds} bed{beds > 1 && "s"}
-          </p>
-          <p className={`flex items-center ${isCompact ? "w-full" : ""} `}>
-            <span className="mr-1">
-              <SurfaceIcon />
-            </span>
-            {surface} m²
-          </p>
-          <p className={`flex items-center ${isCompact ? "w-full" : ""} `}>
-            <span className="mr-1">
-              <LocationIcon />
-            </span>
-            {city}
-          </p>
-        </div>
-      </div>
-      <div className="px-8 pb-8">
-        <div className="flex items-center justify-between my-2">
-          <h1 className="text-color4 text-xl">Total Price</h1>
-          <h5 className="text-color4 text-xl">{formattedPrice ? `$${formattedPrice}` : "Loading..."}</h5>
-        </div>
-        <div className="flex items-center justify-between">
-          <h1 className="text-color2 text-xl">Token Price</h1>
-          <h5 className="text-color2 text-xl">{formattedTokenPrice ? `$${formattedTokenPrice}` : "Loading..."}</h5>
-        </div>
-        <div>
-          <div className="flex items-center justify-between">
-            <div>
-              <h3 className="text-color4 text-xl">Expected Income</h3>
-              <p className="text-color2 text-sm">Not including capital appreciation</p>
-            </div>
-            <h5 className="text-color4 text-lg font-bold">{expectedIncome}%</h5>
-          </div>
-          <div className="flex items-center justify-between text-color4 py-2">
-            <p>Income Start Date</p>
-            <p>{dateIncome}</p>
-          </div>
-          <div className="flex items-center justify-between text-color4">
-            <p>Income per Token</p>
-            <p>$ {tokenIncome} / year</p>
-          </div>
-        </div>
-        <div className="flex justify-center">
-          {/* <Link href={link}> */}
-          <button className="flex items-center justify-between bg-color2 rounded-3xl px-4 py-1 mt-4 w-64 hover:bg-color4 hover:text-white transition-colors duration-300">
-            <h5 className="text-color1 hover:text-white">Available Soon</h5>
-            <ArrowIcon size={24} />
-          </button>
-          {/* </Link> */}
-        </div>
-      </div>
-    </div>
-  );
+			<div className='px-8 py-4'>
+				<div className='flex items-center w-full'>
+					<div className='border-t-2 border-color4 w-8 mr-2 ml-1'></div>
+					<h1 className='text-color4 text-2xl'>{name}</h1>
+				</div>
+				<div
+					className={`text-color4 text-lg flex items-center ${
+						isCompact ? "flex-col space-y-2 items-start" : "space-x-4"
+					} mt-2`}
+				>
+					<p className={`flex items-center ${isCompact ? "w-full" : ""} `}>
+						<span className='mr-1'>
+							<BedIcon size={36} />
+						</span>
+						{beds} bed{beds > 1 && "s"}
+					</p>
+					<p className={`flex items-center ${isCompact ? "w-full" : ""} `}>
+						<span className='mr-1'>
+							<SurfaceIcon />
+						</span>
+						{surface} m²
+					</p>
+					<p className={`flex items-center ${isCompact ? "w-full" : ""} `}>
+						<span className='mr-1'>
+							<LocationIcon />
+						</span>
+						{city}
+					</p>
+				</div>
+			</div>
+			<div className='px-8 pb-8'>
+				<div className='flex items-center justify-between my-2'>
+					<h1 className='text-color4 text-xl'>Total Price</h1>
+					<h5 className='text-color4 text-xl'>
+						{formattedPrice ? `$${formattedPrice}` : "Loading..."}
+					</h5>
+				</div>
+				<div className='flex items-center justify-between'>
+					<h1 className='text-color2 text-xl'>Token Price</h1>
+					<h5 className='text-color2 text-xl'>
+						{formattedTokenPrice ? `$${formattedTokenPrice}` : "Loading..."}
+					</h5>
+				</div>
+				<div>
+					<div className='flex items-center justify-between'>
+						<div>
+							<h3 className='text-color4 text-xl'>Expected Income</h3>
+							<p className='text-color2 text-sm'>
+								Not including capital appreciation
+							</p>
+						</div>
+						<h5 className='text-color4 text-lg font-bold'>{expectedIncome}%</h5>
+					</div>
+					<div className='flex items-center justify-between text-color4 py-2'>
+						<p>Income Start Date</p>
+						<p>{dateIncome}</p>
+					</div>
+					<div className='flex items-center justify-between text-color4'>
+						<p>Income per Token</p>
+						<p>$ {tokenIncome} / year</p>
+					</div>
+				</div>
+				<div className='flex justify-center'>
+					{/* <Link href={link}> */}
+					<button className='flex items-center justify-between bg-color2 rounded-3xl px-4 py-1 mt-4 w-64 hover:bg-color4 hover:text-white transition-colors duration-300'>
+						<h5 className='text-color1 hover:text-white'>Available Soon</h5>
+						<ArrowIcon size={24} />
+					</button>
+					{/* </Link> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomeCardPlus;
