ca7b7b5c41a1f0d5aa99244ed58eed92d4a626fa
adrien.goncalves <adrien.goncalves@etu.u-bordeaux.fr>
2025-02-07 17:35:30 +0100

    Create Types

---
+++ app/types/Commodity.ts
@@ -0,0 +1,7 @@
+export interface Commodity {
+  name: string;
+  image: string;
+  tokenPrice: string;
+  year1Perf: string;
+  years5Perf: string;
+}
---
+++ app/types/House.ts
@@ -0,0 +1,13 @@
+export interface House {
+  id: string;
+  name: string;
+  number: string;
+  surface: string;
+  price: string;
+  city: string;
+  image: string;
+  tokenPrice: string;
+  expectedIncome: string;
+  dateIncome: string;
+  tokenIncome: string;
+}
