a3acb66ea86b63611cecafe0e53cefb860315f45
adrien.goncalves <adrien.goncalves@etu.u-bordeaux.fr>
2025-02-20 18:44:05 +0100

    lint

--- app/marketplace/real-estate/[id]/components/HouseMap.tsx
+++ app/marketplace/real-estate/[id]/components/HouseMap.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 const HouseMap: React.FC = () => {
-  const lat = 19.311014;
-  const lng = -69.543067;
-  const zoom = 18;
-
   return (
     <div className="px-4 sm:px-8 lg:px-16 py-6 w-4/5 mx-auto scroll-none">
       <iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Las%20Terrenas%2032000,%20R%C3%A9publique%20dominicaine+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
