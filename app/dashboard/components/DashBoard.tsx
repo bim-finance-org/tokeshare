b16bc16a8e3632dd8e9c059951cf5f848b2e990a
Adrien <adriengoncalves2003@gmail.com>
2025-04-27 15:12:55 +0200

    remove create section in dashboard

--- app/dashboard/components/DashBoard.tsx
+++ app/dashboard/components/DashBoard.tsx
@@ -3,29 +3,20 @@
 import React, { useState } from 'react'
 import BuyModal from './BuyModal'
 import SellModal from './SellModal'
-import Create from './Create'
 
 const DashBoard = () => {
     const [isBuyOpen, setIsBuyOpen] = useState(true)
     const [isSellOpen, setIsSellOpen] = useState(false)
-    const [isCreateOpen, setIsCreateOpen] = useState(false)
     const handleBuy = () => {
       setIsBuyOpen(true)
       setIsSellOpen(false)
-      setIsCreateOpen(false)
     }
   
     const handleSell = () => {
       setIsBuyOpen(false)
       setIsSellOpen(true)
-      setIsCreateOpen(false)
     }
-  
-    const handleCreate = () => {
-      setIsBuyOpen(false)
-      setIsSellOpen(false)
-      setIsCreateOpen(true)
-    }
+
   
     return (
       <div>
@@ -34,12 +25,10 @@ const DashBoard = () => {
           <div className='flex gap-4 pb-4'>
           <button onClick={handleBuy} className={`bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 ${isBuyOpen ? 'bg-blue-700' : ''}`}>Buy</button>
           <button onClick={handleSell} className={`bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 ${isSellOpen ? 'bg-blue-700' : ''}`}>Sell</button>
-          {/* <button onClick={handleCreate} className='bg-blue-500 text-white px-4 py-2 rounded-md'>Create</button> */}
           </div>
         </div>
         {isBuyOpen && <BuyModal />}
         {isSellOpen && <SellModal />}
-        {/* {isCreateOpen && <Create />} */}
       </div>
     )
 }
