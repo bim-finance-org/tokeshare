cdd57109a0930fba331c42edda08a6f2a11efb14
Noe ROUMEAS <noe.roumeas@outlook.fr>
2025-06-13 18:34:37 +0200

    open explorer on click of Contract

--- components/Contracts.tsx
+++ components/Contracts.tsx
@@ -1,29 +1,18 @@
 import React from "react";
 import Link from "next/link";
 
-const Contracts = ({
-	polygonContract,
-	baseContract,
-}: {
-	polygonContract: string;
-	baseContract: string;
-}) => {
+const Contracts = ({ polygonContract }: { polygonContract: string }) => {
 	return (
 		<div className='flex items-center gap-4'>
 			<h1 className='text-xl text-color4 font-medium'>VIEW SMART CONTRACTS</h1>
 			<div className='flex gap-2'>
 				<Link
-					href={polygonContract}
+					target='_blank'
+					href={"https://polygonscan.com/address/" + polygonContract}
 					className='w-20 flex items-center justify-center px-6 py-2 bg-color2 text-color1 rounded hover:scale-105 transition'
 				>
 					POL
 				</Link>
-				{/* <Link 
-          href={baseContract}
-          className="w-20 flex items-center justify-center px-6 py-2 bg-color4 text-color1 rounded hover:scale-105 transition"
-        >
-          BASE
-        </Link> */}
 			</div>
 		</div>
 	);
