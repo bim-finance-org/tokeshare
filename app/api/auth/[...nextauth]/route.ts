573fe4fb37f576027fb10b9bf27e258f38c51d90
Adrien <adriengoncalves2003@gmail.com>
2025-06-28 11:33:55 +0200

    refacto performance route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 
-// Handler pour NextAuth
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
