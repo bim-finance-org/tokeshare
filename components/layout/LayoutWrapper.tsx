881fa87f0a197b6752b27ab615585311ebdb64f2
Adrien <adriengoncalves2003@gmail.com>
2025-07-30 20:45:56 +0200

    clear useless components

--- components/layout/LayoutWrapper.tsx
+++ components/layout/LayoutWrapper.tsx
@@ -4,17 +4,15 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 import NavBar from './NavBar';
 import Footer from './Footer';
-import { StablecoinPriceInitializer } from '../shared/StablecoinPriceInitializer';
+import { usePrefetchStablePrices } from '@/hooks/useStablePrice';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isDashboardRoute = pathname.startsWith('/dashboard');
+  usePrefetchStablePrices();
 
   return (
     <>
-      {/* Initialise le chargement des prix des stablecoins */}
-      <StablecoinPriceInitializer />
-
       {!isDashboardRoute && <NavBar />}
       {children}
       {!isDashboardRoute && <Footer />}
--- components/shared/FiatTradeWidget.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FiatTradeWidget = () => {
-  return <div>FiatTradeWidget</div>;
-};
-
-export default FiatTradeWidget;
